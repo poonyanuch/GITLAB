1ce19993618232533bfa59d08fb88f3d74ced0b5
LRyusaki <natchapol-t@hotmail.com>
2015-04-28 18:09:48 +0700

    Change code1.c

--- code1.c
+++ code1.c
@@ -10,12 +10,14 @@ int main(int argc,char*argv[])
 
 		printf("child of %s Is %d\n",name,getpid());
  		execl("/bin/ls","ls","-a",(char*)NULL);
+        #change1
 		return 0;
 	}
 	else{
 
 		printf("my child is %d\n",child_pid);
         printf("Hello Worlddd");
+        #change2
 		return(0);
 	    }
 }
