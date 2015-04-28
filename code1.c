a34cc17f0988783af298d11de9aed17870d13359
poonyanuch <poonyanuch-s@hotmail.com>
2015-04-28 18:10:18 +0700

    Change code1.c 2 places

--- code1.c
+++ code1.c
@@ -8,14 +8,14 @@ int main(int argc,char*argv[])
 	if(child_pid == 0)
 	{
 
-		printf("child of %s Is %d\n",name,getpid());
+		printf("The child of %s Is %d\n",name,getpid());
  		execl("/bin/ls","ls","-a",(char*)NULL);
 		return 0;
 	}
 	else{
 
 		printf("my child is %d\n",child_pid);
-        printf("Hello Worlddd");
+        printf("Hello Worlddd!");
 		return(0);
 	    }
 }
